4988e3eb9eeb21b03a81b2e1de2ee8ef6ca2c169
Angel Sevilla <asevilla@um.es>
2021-07-20 12:39:49 +0000

    function fixed

---
+++ bda-03-function.py
@@ -0,0 +1,21 @@
+"""
+    Script to print a Vegetable Soup Recipe using a function
+""" 
+
+def print_message(input_message):
+    print(input_message)
+
+message = """
+Ingredients Vegetable Soup Recipe:
+ - a pumpkin
+ - five tomatoes
+ - two onions
+ - bouillon
+ - water
+ - salt
+ - secret ingredient
+ - additional ingredient
+"""
+
+# Substitude XXXX with the right variable to print the previous message
+print_message(XXXX)
