3623c4649f4bce919d8436e475f74bbbf5966da5
Angel Sevilla <asevilla@um.es>
2021-07-22 13:22:30 +0000

    original

--- bda-02-conditional.py
+++ bda-02-conditional.py
@@ -2,7 +2,7 @@
     Script to print with a condition
 """ 
 # Substitude XXXX with the right value to print 'Adult' or 'Minor'
-age = 18
+age = XXXX
 print('Age:', age)
 if age >= 18:
     print('Adult')
